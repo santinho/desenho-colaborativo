15f914174d527fdc6793c4f439d4f43f740d2a2e
Leandro Santinho <1511401+santinho@users.noreply.github.com>
2025-07-31 00:24:50 -0300

    Retirada de alert

--- src/main/resources/META-INF/resources/script.js
+++ src/main/resources/META-INF/resources/script.js
@@ -329,7 +329,7 @@ class DrawingGame {
             default:
                 if (message.error) {
                     console.error('WebSocket error:', message.error);
-                    alert(message.error);
+                    //alert(message.error);
                 }
         }
     }
